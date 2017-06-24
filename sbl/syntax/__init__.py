76b49cfc0ce99657f691ac8b4a6e76399786685b
Alek Ratzloff <alekratz@gmail.com>
2017-06-24 11:50:54 -0400

    Move AST items to their own AST module.
    This move is in preparation for adding more data types beyond integers
    to allow for pushing to the stack.
    
    This really shouldn't conflict with python's standard AST module, but
    I've been wrong before.
    
    Signed-off-by: Alek Ratzloff <alekratz@gmail.com>

--- sbl/syntax/__init__.py
+++ sbl/syntax/__init__.py
@@ -1,2 +1,3 @@
 from . parse import *
+from . ast import *
 from . token import *
--- sbl/syntax/parse.py
+++ sbl/syntax/parse.py
@@ -1,55 +1,5 @@
 from . token import *
-
-
-class Item:
-    def __init__(self, rng, val):
-        self.range = rng
-        self.val = val
-
-    def __repr__(self):
-        return repr(self.val)
-
-
-class Action:
-    def __init__(self, rng, items, pop=False):
-        self.range = rng
-        self.items = items
-        self.pop = pop
-
-
-class Branch:
-    """
-    A branch statement. A branch may optionally have a trailing "el" block as
-    well.
-    :br: the list of lines held by the br block.
-    :el: the list of lines held by the el block.
-    """
-    def __init__(self, rng, br_block, el_block):
-        self.range = rng
-        self.br_block = br_block
-        self.el_block = el_block
-
-Line = Union[Action, Branch]
-
-
-class Block:
-    def __init__(self, rng, lines: List[Line]):
-        self.range = rng
-        self.lines = lines
-
-    def __iter__(self):
-        for it in self.lines:
-            yield it
-
-
-class FunDef:
-    def __init__(self, rng, name: str, block: Block):
-        self.range = rng
-        self.name = name
-        self.block = block
-
-
-Source = List[FunDef]
+from . ast import *
 
 
 class Parser:
